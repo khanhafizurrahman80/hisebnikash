ba6ae217ab17a2ecce3e9b7a2fd76bb6a07b0398
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-04-27 17:35:04 +0200

    initialize project

---
+++ src/main/java/com/example/spring/learning/HisebnikashApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisebnikashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisebnikashApplication.class, args);
+	}
+
+}
