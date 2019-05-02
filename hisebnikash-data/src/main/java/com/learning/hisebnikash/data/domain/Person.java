7312b339e46227a957b0a12fa85a4e0b1cefe4a9
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-05-02 14:52:58 +0200

    entity class is changed from constructor to getter and setter

--- hisebnikash-data/src/main/java/com/learning/hisebnikash/data/domain/Person.java
+++ hisebnikash-data/src/main/java/com/learning/hisebnikash/data/domain/Person.java
@@ -19,11 +19,44 @@ public class Person {
     private String email;
     private LocalDate createdDate;
 
-    public Person(Long id, String firstName, String lastName, String email, LocalDate createdDate) {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
         this.createdDate = createdDate;
     }
 
